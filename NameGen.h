f91e49839ebcc6ba3bbdfcec619ef304c3724dbc
OdenDillenkoffer <odillenkoffer@gmail.com>
2024-11-29 22:06:03 -0500

    further work

--- NameGen.h
+++ NameGen.h
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <cstdlib>
 #include <utility>
+#include <Jobs.h>
 #pragma
 std::vector<std::pair<std::string, std::string>> NameGenerator(){
     //takes the names in names.txt provided by
@@ -29,3 +30,17 @@ int Rand2 = rand() % 1001;
 auto Pair = std::make_pair(name[Rand1].first, name[Rand2].second);
 return Pair;
 }
+
+Job Janitor("Janitor", 0, 24, 4, 8, 12);
+Job Pilot("Pilot", 0, 24, 2, 10, 50);
+Job CustomerService("Customer Service", 9, 17, 4, 8, 13);
+Job CEO("CEO", 9, 17, 8, 12, 300);
+Job Engineer("Engineer", 9, 17, 8, 12, 50);
+Job Security("Security", 0, 24, 4, 8, 15);
+Job BaggageHandle("Baggage Handler", 0, 24, 4, 8, 12);
+Job Fueler("Fueler", 0 ,24, 4, 12, 13);
+Job Mechanic("Mechanic", 5, 24, 4, 12, 20);
+Job RampSup("Ramp Supplier", 0 ,24, 4, 8, 13);
+Job Manager("Manager", 9, 20, 4, 10, 35);
+Job SpaceTrafficController("Space Traffic Controller", 0, 24, 4, 8, 40);
+
---
+++ graphImp.h
@@ -0,0 +1,8 @@
+#include <Jobs.h>
+#include <NameGen.h>
+#pragma
+class GraphNode{
+    public:
+    int workID;
+    std::pair<std::pair<std::string, std::string>, Job> Information;
+};
