126139af46aedb9f8ae6268e8a316999ad11e8c7
Jaden Edgecombe <38144903+jadenedgecombe@users.noreply.github.com>
2024-12-02 23:27:31 -0500

    Update NameGen.h
    Comments can be found inside. (JE)

--- NameGen.h
+++ NameGen.h
@@ -4,31 +4,33 @@
 #include <sstream>
 #include <cstdlib>
 #include <utility>
-#include <Jobs.h>
-#pragma
-std::vector<std::pair<std::string, std::string>> NameGenerator(){
-    //takes the names in names.txt provided by
-    // 1000randomnames
-    // it will randomly generate a name from those
-    std::ifstream nameFile("names.txt");
+#include "Jobs.h" // JE: changed the <> to ""
+
+#pragma once // JE: added "once"
+
+std::vector<std::pair<std::string, std::string>> NameGenerator(){ 
+    std::ifstream nameFile("names.txt"); // JE: takes the names from name.txt (generated randomly from 1000 randomnames)
     std::vector<std::pair<std::string, std::string>> names;
     std::string line;
-    while(std::getline(nameFile, line)){
+    
+    while(std::getline(nameFile, line)){ // JE: splits the names and stores them as pairs in the vector names
         std::istringstream iss(line);
         std::string FirstName, LastName;
         if(iss >> FirstName >> LastName){
             names.emplace_back(FirstName, LastName);
         }
     }
+    
     nameFile.close();
-    return names;
-
+    return names; // JE: returns names (contains all the names) 
 }
-std::pair<std::string, std::string> randName(std::vector<std::pair<std::string, std::string>> name){
-int Rand1 = rand() % 1001;
-int Rand2 = rand() % 1001;
-auto Pair = std::make_pair(name[Rand1].first, name[Rand2].second);
-return Pair;
+
+std::pair<std::string, std::string> randName(std::vector<std::pair<std::string, std::string>> name){ // JE: adjusted indentation and added whitespace
+    int Rand1 = rand() % 1001;
+    int Rand2 = rand() % 1001;
+    auto Pair = std::make_pair(name[Rand1].first, name[Rand2].second);
+    
+    return Pair;
 }
 
 
