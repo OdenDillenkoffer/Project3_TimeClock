14fc9986290d376e77fae7cb5656e56772c773f4
Jaden Edgecombe <38144903+jadenedgecombe@users.noreply.github.com>
2024-12-03 18:04:13 -0500

    Updated graph.h
    Fixed syntax. Enhanced code readability by adding whitespace and adjusting indentation.

--- graph.h
+++ graph.h
@@ -1,16 +1,21 @@
 #include <unordered_map>
-#include <Nodes.h>
-#pragma
-class Graph{
-public:
-std::unordered_map<int, std::vector<std::pair<Task, int>>> AdjList;
-//int is Task ID
-//vector is connect
-void AddNode(Task& node){AdjList[node.taskID] = {};}
-void AddEdge(Task& srcTask, Task& nextTask){
-    AdjList[srcTask.taskID].push_back(std::make_pair(nextTask, srcTask.TimeToComplete));
+#include "Nodes.h" // JE: changed <> to ""
+#pragma once
+
+class Graph { // JE: fixed indentation and added whitespace
+    public:
+    std::unordered_map<int, std::vector<std::pair<Task, int>>> AdjList;
+    //int is Task ID
+    //vector is connect
+    void AddNode(Task& node) {
+        AdjList[node.taskID] = {};
     }
-std::vector<std::pair<Task, int>> getNeighbors(int id){
-    return AdjList[id];
-}
+
+    void AddEdge(Task& srcTask, Task& nextTask) {
+        AdjList[srcTask.taskID].push_back(std::make_pair(nextTask, srcTask.TimeToComplete));
+    }
+
+    std::vector<std::pair<Task, int>> getNeighbors(int id) {
+        return AdjList[id];
+    }
 };
